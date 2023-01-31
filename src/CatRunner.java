6c65e489eb89221a37e7042649452c8f411c1e1b
Imran Fazilov <ifazilov3584@bths.edu>
2023-01-31 12:27:15 -0500

    period end

--- src/CatRunner.java
+++ src/CatRunner.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class CatRunner {
+import java.util.ArrayList;
+
+public class CatRunner {
+    public static void main(String[] args) {
+        ArrayList<Cat> cats = new ArrayList<Cat>();
+        Cat cat1 = new Cat("Stryder");
+        Cat cat2 = new Cat("Joe");
+        Cat cat3 = new Cat("Det");
+        cats.add(cat1);
+        cats.add(cat2);
+        cats.add(cat3);
+        System.out.println(cats);
+
+        Cat cat4 = new Cat("Paul");
+        Cat replaceCat = cats.set(2, cat4);
+        System.out.println(cats);
+        System.out.println(replaceCat);
+        cat4.setName(cat4.getName() + " Meow");
+        cats.set(2, cat4);
+        System.out.println(cat4);
+        System.out.println(cats);
+    }
 }
--- src/MainRunner.java
+++ src/MainRunner.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class MainRunner {
+import java.util.ArrayList;
+
+public class MainRunner {
+    public static void main(String[] args) {
+        ArrayList<String> trees = new ArrayList<String>();
+        trees.add("pine");
+        trees.add("maple");
+        trees.add("frasier fir");
+        System.out.println(trees);
+        System.out.println(trees.size());
+        System.out.println(trees.get(1));
+
+
+        ArrayList<String> names = new ArrayList<String>();
+        names.add("Ben");
+        names.add("Andy");
+        names.add("David");
+        String[] moreNames = {"Crystal", "Monica", "Toby", "Nancy"};
+        int namesAmt = names.size();
+        int moreNamesAmt = moreNames.length;
+        int thirdNameLength = names.get(2).length();
+        int secondNameLength = moreNames[1].length();
+        int lengths = namesAmt + moreNamesAmt + thirdNameLength + secondNameLength;
+        System.out.println(lengths); // should print 18
+
+    }
 }
